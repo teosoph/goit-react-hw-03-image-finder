c1f77ddac5cec5f297e9a96c7085891c45475281
teosoph <39537001+teosoph@users.noreply.github.com>
2021-07-27 01:07:51 +0300

    HW-03___tasks

---
+++ image-finder/src/ImageGalleryItem/ImageGalleryItem.js
@@ -0,0 +1,6 @@
+// Описание компонента ImageGalleryItem
+// Компонент элемента списка с изображением. Создает DOM-элемент следующей структуры.
+
+// <li className="ImageGalleryItem">
+//   <img src="" alt="" className="ImageGalleryItem-image" />
+// </li>
